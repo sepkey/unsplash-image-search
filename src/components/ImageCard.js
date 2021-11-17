1944c02bfb711ad1ae2a796d8f479dd1bb9189ac
sepkey <sepidekia6990@gmail.com>
2021-11-17 13:50:09 +0300

    V2-Tile of images

---
+++ src/components/ImageCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+class ImageCard extends React.Component {
+  constructor(props) {
+    super();
+    this.imgRef = React.createRef();
+    this.state = { spans: 0 };
+  }
+  componentDidMount() {
+    // console.log(this.imgRef);
+    this.imgRef.current.addEventListener("load", this.setSpans);
+  }
+  setSpans = () => {
+    // console.log(this.imgRef.current.clientHeight);
+    const height = this.imgRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    console.log(spans);
+
+    this.setState({ spans });
+  };
+
+  render() {
+    const { alt_description, urls } = this.props.image;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imgRef} alt={alt_description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
--- src/components/Imagelist.js
+++ src/components/Imagelist.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 const ImageList = (props) => {
-  console.log(props.images);
-  const images = props.images.map(({ urls, alt_description, id }) => {
-    return <img src={urls.regular} alt={alt_description} key={id} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
